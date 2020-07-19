29462b3e0e74799b45f01f5430375ed448a2295e
Giles Perry <giles@gilesperry.info>
2020-07-19 22:59:44 +0100

    Remove icon from default button

--- code/Button.tsx
+++ code/Button.tsx
@@ -36,6 +36,7 @@ const defaultProps = {
     variant: "primary",
     isIconSearch: true,
     searchPhrase: "plus",
+    hasTrailingIcon: null,
     // link: false,
     // href: null,
     // blank: false,
