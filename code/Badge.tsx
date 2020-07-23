8ec8c125671d68eca71a645dd87b424eb0867614
Giles Perry <giles@gilesperry.info>
2020-07-23 17:19:13 +0100

    Add Icons to Badge

--- code/Badge.tsx
+++ code/Badge.tsx
@@ -1,7 +1,14 @@
 import * as React from "react"
 import { addPropertyControls, ControlType } from "framer"
+
 // @ts-ignore
 import BpkBadge, { BADGE_TYPES } from "backpack-transpiled/bpk-component-badge"
+// @ts-ignore
+import * as Icons from "backpack-transpiled/bpk-component-icon/all"
+
+import { findIcon } from "./Icon"
+
+const iconNames = Object.keys(Icons.sm)
 
 interface Props {
     text?: string
@@ -14,6 +21,11 @@ interface Props {
         | "outline"
     centered?: boolean
     docked?: "left" | "right"
+    _hasIcon?: boolean
+    _isIconSearch?: boolean
+    _chosenIcon?: string
+    _searchPhrase?: string
+    onClick?: any
 }
 
 const defaultProps: Props = {
@@ -21,12 +33,32 @@ const defaultProps: Props = {
     type: BADGE_TYPES.warning,
     centered: false,
     docked: null,
+    _hasIcon: true,
+    _isIconSearch: false,
+    _chosenIcon: "flight",
+    _searchPhrase: "flight",
 }
 
 export function Badge(props: Props) {
-    const { text, ...rest } = props
+    const {
+        text,
+        _hasIcon,
+        _isIconSearch,
+        _chosenIcon,
+        _searchPhrase,
+        ...rest
+    } = props
 
-    return <BpkBadge {...rest}>{text}</BpkBadge>
+    const iconName = _isIconSearch ? findIcon(_searchPhrase) : _chosenIcon
+
+    const Icon = Icons.sm[iconName]
+
+    return (
+        <BpkBadge {...rest}>
+            {_hasIcon ? <Icon /> : null}
+            {text}
+        </BpkBadge>
+    )
 }
 
 Badge.defaultProps = defaultProps
@@ -48,14 +80,55 @@ addPropertyControls(Badge, {
             BADGE_TYPES.outline,
         ],
     },
-    centered: {
-        type: ControlType.Boolean,
-        title: "Centered",
-    },
+    // centered: {
+    //     type: ControlType.Boolean,
+    //     title: "Centered",
+    // },
     docked: {
         type: ControlType.Enum,
         title: "Docked",
         options: [null, "left", "right"],
-        optionTitles: ["None", "Left", "Right"],
+        optionTitles: ["No", "Left", "Right"],
+        displaySegmentedControl: true,
     },
+    // Icon Controls
+    _hasIcon: {
+        type: ControlType.Boolean,
+        title: "Show Icon",
+        defaultValue: false,
+        enabledTitle: "Yes",
+        disabledTitle: "No",
+    },
+    _isIconSearch: {
+        type: ControlType.Boolean,
+        title: "Find Icon",
+        defaultValue: false,
+        enabledTitle: "Search",
+        disabledTitle: "Choose",
+        hidden(props) {
+            return props._hasIcon === null
+        },
+    },
+    _chosenIcon: {
+        type: ControlType.Enum,
+        title: "Icon Name",
+        defaultValue: "plus",
+        options: iconNames,
+        optionTitles: iconNames.map((key) => Icons.sm[key]),
+        hidden(props) {
+            return props._isIconSearch === true || props._hasIcon === false
+        },
+    },
+    _searchPhrase: {
+        type: ControlType.String,
+        title: "Icon Name",
+        defaultValue: "plus",
+        placeholder: "None",
+        hidden(props) {
+            return props._isIconSearch === false || props._hasIcon === false
+        },
+    },
+    // onClick: {
+    //     type: ControlType.EventHandler,
+    // },
 })
