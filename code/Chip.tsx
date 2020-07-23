88f610fe8ede5635a3391f08badbfed6103bf765
Giles Perry <giles@gilesperry.info>
2020-07-23 11:21:06 +0100

    Add dismissible prop and onClose event to Chip

--- code/Chip.tsx
+++ code/Chip.tsx
@@ -5,13 +5,18 @@ import { addPropertyControls, ControlType } from "framer"
 import BpkChip, { CHIP_TYPES } from "backpack-transpiled/bpk-component-chip"
 
 interface Props {
+    height?: number
     text?: string
     type?: "neutral" | "primary"
+    dismissible?: boolean
+    onClose?: any
 }
 
 const defaultProps: Props = {
+    height: 36,
     text: "Chip",
     type: CHIP_TYPES.neutral,
+    dismissible: true,
 }
 
 export function Chip(props: Props) {
@@ -31,5 +36,16 @@ addPropertyControls(Chip, {
         type: ControlType.Enum,
         title: "Type",
         options: [CHIP_TYPES.neutral, CHIP_TYPES.primary],
+        defaultValue: CHIP_TYPES.neutral,
+    },
+    dismissible: {
+        type: ControlType.Boolean,
+        title: "Dismissible",
+        defaultValue: true,
+        enabledTitle: "Yes",
+        disabledTitle: "No",
+    },
+    onClose: {
+        type: ControlType.EventHandler,
     },
 })
