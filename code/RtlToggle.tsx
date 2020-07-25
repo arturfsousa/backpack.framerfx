8ec40901b865e5ea58eafbf46bf8c55807ff8c21
Giles Perry <giles@gilesperry.info>
2020-07-25 18:05:14 +0100

    Add RtlToggle

---
+++ code/RtlToggle.tsx
@@ -0,0 +1,13 @@
+import * as React from "react"
+
+// @ts-ignore
+import BpkRtlToggle from "backpack-transpiled/bpk-component-rtl-toggle"
+
+export function RtlToggle(props) {
+    return <BpkRtlToggle />
+}
+
+RtlToggle.defaultProps = {
+    width: 60,
+    height: 20,
+}
