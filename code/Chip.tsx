a382fd0b38bc3cfa6b1925374a36ab3d26f7df17
Giles Perry <giles@gilesperry.info>
2020-08-04 09:23:00 +0100

    Add support for icons in Chip

--- code/Chip.tsx
+++ code/Chip.tsx
@@ -7,13 +7,39 @@ import BpkSelectableChip, {
     // @ts-ignore
 } from "backpack-transpiled/bpk-component-chip"
 
+// @ts-ignore
+import * as Icons from "backpack-transpiled/bpk-component-icon/all"
+
+import { findIcon } from "./Icon"
+import { indentTitle } from "./lib/indentTitle"
+
+const iconNames = Object.keys(Icons.sm)
 const chipTypes = Object.keys(CHIP_TYPES)
 
 export function Chip(props) {
-    const { _isSelectable, _isntSelected, text, onClick, ...rest } = props
+    const {
+        _selectableIconPosition,
+        _isDismissibleWithoutIcon,
+        _isIconSearch,
+        _chosenIcon,
+        _searchPhrase,
+        _isSelectable,
+        _isntSelected,
+        text,
+        onClick,
+        ...rest
+    } = props
 
-    const [clicked, setClicked] = React.useState(_isSelectable && !_isntSelected)
-    React.useEffect(() => setClicked(_isSelectable && !_isntSelected), [_isSelectable, _isntSelected])
+    const iconName = _isIconSearch ? findIcon(_searchPhrase) : _chosenIcon
+    const Icon = Icons.sm[iconName]
+
+    const [clicked, setClicked] = React.useState(
+        _isSelectable && !_isntSelected
+    )
+    React.useEffect(() => setClicked(_isSelectable && !_isntSelected), [
+        _isSelectable,
+        _isntSelected,
+    ])
 
     const handleClick = () => {
         setClicked(!clicked)
@@ -24,6 +50,12 @@ export function Chip(props) {
         return (
             <BpkSelectableChip
                 {...rest}
+                leadingAccessoryView={
+                    _selectableIconPosition === "left" ? <Icon /> : null
+                }
+                trailingAccessoryView={
+                    _selectableIconPosition === "right" ? <Icon /> : null
+                }
                 accessibilityLabel="Toggle"
                 selected={clicked}
                 onClick={handleClick}
@@ -35,13 +67,14 @@ export function Chip(props) {
         return (
             <div
                 style={
-                    clicked
-                        ? { display: "none" }
-                        : { display: "inline-block" }
+                    clicked ? { display: "none" } : { display: "inline-block" }
                 }
             >
                 <BpkDismissibleChip
                     {...rest}
+                    leadingAccessoryView={
+                        _isDismissibleWithoutIcon ? null : <Icon />
+                    }
                     accessibilityLabel="Close"
                     onClick={handleClick}
                 >
@@ -56,6 +89,11 @@ Chip.defaultProps = {
     height: 36,
     text: "Chip",
     type: CHIP_TYPES.primary,
+    _selectableIconPosition: null,
+    _isDismissibleWithoutIcon: true,
+    _isIconSearch: false,
+    _chosenIcon: "flight",
+    _searchPhrase: "flight",
 }
 
 addPropertyControls(Chip, {
@@ -78,12 +116,83 @@ addPropertyControls(Chip, {
     },
     _isntSelected: {
         type: ControlType.Boolean,
-        title: "State",
+        title: indentTitle("State"),
         defaultValue: true,
         enabledTitle: "Default",
         disabledTitle: "Selected",
         hidden: ({ _isSelectable }) => !_isSelectable,
     },
+    // Icon Controls
+    _selectableIconPosition: {
+        type: ControlType.Enum,
+        title: "Icon",
+        defaultValue: null,
+        optionTitles: ["None", "Left", "Right"],
+        options: [null, "left", "right"],
+        displaySegmentedControl: true,
+        hidden: ({ _isSelectable }) => !_isSelectable,
+    },
+    _isDismissibleWithoutIcon: {
+        type: ControlType.Boolean,
+        title: "Icon",
+        defaultValue: true,
+        enabledTitle: "None",
+        disabledTitle: "Left",
+        hidden: ({ _isSelectable }) => _isSelectable,
+    },
+    _isIconSearch: {
+        type: ControlType.Boolean,
+        title: indentTitle("Find Icon"),
+        defaultValue: false,
+        enabledTitle: "Search",
+        disabledTitle: "Choose",
+        hidden({
+            _isSelectable,
+            _selectableIconPosition,
+            _isDismissibleWithoutIcon,
+        }) {
+            const hasIcon =
+                (_isSelectable && _selectableIconPosition !== null) ||
+                (!_isSelectable && !_isDismissibleWithoutIcon)
+            return !hasIcon
+        },
+    },
+    _chosenIcon: {
+        type: ControlType.Enum,
+        title: indentTitle("Icon Name"),
+        defaultValue: "flight",
+        options: iconNames,
+        optionTitles: iconNames.map((key) => Icons.sm[key]),
+        hidden({
+            _isIconSearch,
+            _isSelectable,
+            _selectableIconPosition,
+            _isDismissibleWithoutIcon,
+        }) {
+            const hasIcon =
+                (_isSelectable && _selectableIconPosition !== null) ||
+                (!_isSelectable && !_isDismissibleWithoutIcon)
+            return _isIconSearch || !hasIcon
+        },
+    },
+    _searchPhrase: {
+        type: ControlType.String,
+        title: indentTitle("Icon Name"),
+        defaultValue: "flight",
+        placeholder: "None",
+        hidden({
+            _isIconSearch,
+            _isSelectable,
+            _selectableIconPosition,
+            _isDismissibleWithoutIcon,
+        }) {
+            const hasIcon =
+                (_isSelectable && _selectableIconPosition !== null) ||
+                (!_isSelectable && !_isDismissibleWithoutIcon)
+            return !_isIconSearch || !hasIcon
+        },
+    },
+    // Events
     onClick: {
         type: ControlType.EventHandler,
     },
