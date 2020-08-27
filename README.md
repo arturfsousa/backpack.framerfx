471ad9cf5328cbcbef9f74902c6d8c27325c2fa4
Giles Perry <giles@gilesperry.info>
2020-08-27 09:48:42 +0100

    Add Switch component

--- README.md
+++ README.md
@@ -71,7 +71,7 @@ and open the project folder `backpack.framerfx` in `Framer`.
 | [`bpk-component-slider`](https://backpack.github.io/components/slider/?platform=web)                                       | 👍        |
 | [`bpk-component-spinner`](https://backpack.github.io/components/spinner/?platform=web)                                     | 👍        |
 | [`bpk-component-star-rating`](https://backpack.github.io/components/star-rating/?platform=web)                             | 👍        |
-| [`bpk-component-switch`](https://backpack.github.io/components/switch/?platform=web)                                       | -         |
+| [`bpk-component-switch`](https://backpack.github.io/components/switch/?platform=web)                                       | 👍        |
 | [`bpk-component-table`](https://backpack.github.io/components/table/?platform=web)                                         | -         |
 | [`bpk-component-text`](https://backpack.github.io/components/text/?platform=web)                                           | 👍        |
 | [`bpk-component-textarea`](https://github.com/Skyscanner/backpack/tree/master/packages/bpk-component-textarea)             | 👍        |
---
+++ code/Switch.tsx
@@ -0,0 +1,79 @@
+/*
+ * Backpack - Skyscanner's Design System
+ *
+ * Copyright 2016-2020 Skyscanner Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import * as React from "react"
+import { addPropertyControls, ControlType } from "framer"
+
+// @ts-ignore
+import BpkSwitch, {
+    SWITCH_TYPES,
+} from "backpack-transpiled/bpk-component-switch"
+
+const switchTypes = Object.keys(SWITCH_TYPES)
+
+export function Switch(props) {
+    const { label, type, onChange } = props
+
+    const [checked, setChecked] = React.useState(props.checked)
+    React.useEffect(() => setChecked(props.checked), [props.checked])
+
+    const handleChange = () => {
+        const state = !checked
+        setChecked(state)
+        onChange && onChange(state)
+    }
+
+    return (
+        <BpkSwitch
+            type={type}
+            onChange={handleChange}
+            label={label}
+            checked={checked}
+        />
+    )
+}
+
+Switch.defaultProps = {
+    height: 30,
+    width: 240,
+    checked: true,
+    label: "Add nearby airports",
+}
+
+addPropertyControls(Switch, {
+    type: {
+        type: ControlType.Enum,
+        title: "Type",
+        options: switchTypes,
+    },
+    label: {
+        title: "Label",
+        type: ControlType.String,
+        defaultValue: "Add nearby airports",
+    },
+    checked: {
+        type: ControlType.Boolean,
+        title: "State",
+        defaultValue: true,
+        enabledTitle: "On",
+        disabledTitle: "Off",
+    },
+    onChange: {
+        type: ControlType.EventHandler,
+    },
+})
